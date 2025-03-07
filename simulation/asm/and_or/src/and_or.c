652283cfd122a10648a918b67fb0eee75cf17304
Krishna010501 <krishnai.pegasian@gmail.com>
2025-03-07 08:45:15 -0500

    implement AND and OR operations

---
+++ simulation/asm/and_or/src/and_or.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int and_operation(int a, int b) {
+    return a & b;
+}
+
+int or_operation(int a, int b) {
+    return a | b;
+}
---
+++ simulation/asm/and_or/src/and_or.h
@@ -0,0 +1,7 @@
+#ifndef AND_OR_H
+#define AND_OR_H
+
+int and_operation(int a, int b);
+int or_operation(int a, int b);
+
+#endif
