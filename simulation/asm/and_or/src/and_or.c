cd1de3617f1fd4168a72b47c233ec33ae03da678
Krishna Pavan Kalyan Inturi <krishnai.pegasian@gmail.com>
2025-03-28 21:25:23 +0300

    Delete simulation/asm/and_or directory

--- simulation/asm/and_or/src/and_or.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int and_operation(int a, int b) {
-    return a & b;
-}
-
-int or_operation(int a, int b) {
-    return a | b;
-}
--- simulation/asm/and_or/src/and_or.h
+++
@@ -1,7 +0,0 @@
-#ifndef AND_OR_H
-#define AND_OR_H
-
-int and_operation(int a, int b);
-int or_operation(int a, int b);
-
-#endif
