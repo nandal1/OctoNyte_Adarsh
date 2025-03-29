398020d8d7535889d57c010ee10b4d9dc55d1d93
Krishna Pavan Kalyan Inturi <krishnai.pegasian@gmail.com>
2025-03-29 21:15:55 +0300

    Delete simulation/run/obj_dir directory

--- simulation/run/obj_dir/verilator_top.d
+++
@@ -1,6 +0,0 @@
-verilator_top.o: ../verilator_top.cpp ../verilator_interface.h \
- ../obj_dir/Vtop.h /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_config.h \
- /usr/share/verilator/include/verilated_types.h \
- /usr/share/verilator/include/verilated_funcs.h
