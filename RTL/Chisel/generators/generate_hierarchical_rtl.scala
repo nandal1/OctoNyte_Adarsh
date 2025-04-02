2ac01cd26eafb0f46ef8ee8c6cb49831ceaeb64f
Nikhil6784 <nkukkamalla@rivier.edu>
2025-04-02 15:45:37 -0400

    storeunit.v

--- RTL/Chisel/generators/generate_hierarchical_rtl.scala
+++ RTL/Chisel/generators/generate_hierarchical_rtl.scala
@@ -1,4 +1,4 @@
-// Run from RTL/Chisel sbt 'runMain generators.GenerateHardware'
+// Run from RTL/Chisel sbt 'runMain generators.GenerateHierarchicalRTL'
 
 // Licensed under the BSD 3-Clause License. 
 // See https://opensource.org/licenses/BSD-3-Clause for details.
@@ -261,7 +261,7 @@ object GenerateHierarchicalRTL extends App {
             prep -top $moduleName;                         
             techmap;
             dfflibmap -liberty $skywaterPdkLib; 
-            abc -liberty $skywaterPdkLib;   
+            abc -liberty $skywaterPdkLib-script +abc_map_new_area.script;  
             opt_clean;
             stat;
             write_verilog -noattr $verilogOutFile
