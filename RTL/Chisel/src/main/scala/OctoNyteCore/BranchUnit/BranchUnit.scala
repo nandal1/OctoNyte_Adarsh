ee14a81af6f24abbcdf66cc4bd6b98af009451a9
Adarsh Nandal <97931902+nandal1@users.noreply.github.com>
2025-04-02 21:30:46 +0000

    Updated Next PC

--- RTL/Chisel/src/main/scala/OctoNyteCore/BranchUnit/BranchUnit.scala
+++ RTL/Chisel/src/main/scala/OctoNyteCore/BranchUnit/BranchUnit.scala
@@ -1,4 +1,4 @@
-// // Licensed under the BSD 3-Clause License.
+// Licensed under the BSD 3-Clause License.
 // See https://opensource.org/licenses/BSD-3-Clause for details.
 
 package OctoNyte.BranchUnit
@@ -19,6 +19,7 @@ class BranchUnit extends Module {
     // Outputs
     val taken = Output(Bool())       // Branch taken signal
     val target = Output(UInt(32.W))  // 32-bit target address
+    val nextPc = Output(UInt(32.W))  // Next PC value
     val misaligned = Output(Bool())  // Alignment check flag
   })
 
@@ -29,8 +30,7 @@ class BranchUnit extends Module {
   // Alignment check (RV32I requires 32-bit aligned branches)
   io.misaligned := targetAddr(1, 0) =/= 0.U
 
-  
-  // Branch condition evaluation using nested Mux like StoreUnit
+  // Branch condition evaluation
   io.taken := Mux(io.branchOp === 0.U, io.rs1 === io.rs2,          // BEQ
               Mux(io.branchOp === 1.U, io.rs1 =/= io.rs2,          // BNE
               Mux(io.branchOp === 4.U, io.rs1.asSInt < io.rs2.asSInt,  // BLT
@@ -38,5 +38,7 @@ class BranchUnit extends Module {
               Mux(io.branchOp === 6.U, io.rs1 < io.rs2,            // BLTU
               Mux(io.branchOp === 7.U, io.rs1 >= io.rs2,           // BGEU
               false.B)))))) && io.valid && !io.misaligned
-}
 
+  //Next PC calculation
+  io.nextPc := Mux(io.taken, io.target, io.pc + 4.U)
+}
