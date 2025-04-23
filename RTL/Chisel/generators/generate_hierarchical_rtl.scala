a381907d01c536ac24796ccad12a91936ea2038d
Nikhil6784 <nkukkamalla@rivier.edu>
2025-04-23 11:47:53 -0400

    aky130_hd_area and performance

--- RTL/Chisel/generators/generate_hierarchical_rtl.scala
+++ RTL/Chisel/generators/generate_hierarchical_rtl.scala
@@ -265,7 +265,7 @@ object GenerateHierarchicalRTL extends App {
             prep -top $moduleName;                         
             techmap;
             dfflibmap -liberty $skywaterPdkLib; 
-            abc -liberty $skywaterPdkLib;  
+            abc  -liberty $skywaterPdkLib;  
             opt_clean;
             stat;
             write_verilog -noattr $verilogOutFile
