09be3cb456684a0c7e26fb1f42ace8ab930f0fd6
Nikhil6784 <nkukkamalla@rivier.edu>
2025-04-23 11:52:21 -0400

    sky130_hs

--- RTL/Chisel/generators/generate_hierarchical_rtl.scala
+++ RTL/Chisel/generators/generate_hierarchical_rtl.scala
@@ -67,7 +67,7 @@ object GenerateHierarchicalRTL extends App {
   val generatedNetlistPath = "generators/generated/netlist"
   val generatedDiagramsPath = "generators/generated/diagrams"
 
-  val skywaterPdkLib = "../../synthesis/pdk/sky130_fd_sc_hd/timing/sky130_fd_sc_hd__tt_025C_1v80.lib"
+  val skywaterPdkLib = "../../synthesis/pdk/sky130_fd_sc_hs/timing/sky130_fd_sc_hs__tt_025C_1v80.lib.part1"
   val optimizeForASIC = true
 
  
